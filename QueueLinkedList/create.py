3e5e72ebd12ed7090206defe3abaf41f3ad0d3f5
Shabab007 <shabab.23rhythm@gmail.com>
2023-12-27 15:33:39 +0600

    queue completed

---
+++ QueueLinkedList/create.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self,value) -> None:
+        self.value=value
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail= None
+    def __iter__(self):
+        currNode= self.head
+        while currNode:
+            yield currNode
+            currNode = currNode.next
+class Queue:
+    def __init__(self):
+     self.linkedList = LinkedList()
+    def __str__(self) -> str:
+     values=[str(x.value) for x in self.linkedList]
+     return " ".join(values)
+    def is_empty(self):
+       if self.linkedList.head is None:
+          return True
+       else:
+          return False
+    def enqueue(self,value):
+       newNode = Node(value)
+       if self.linkedList.head is None:
+          self.linkedList.head = newNode
+          self.linkedList.tail = newNode
+       else:
+          self.linkedList.tail.next = newNode
+          self.linkedList.tail = newNode
+    def dequeue(self):
+       if self.is_empty():
+          return "Queue is empty"
+       else:
+        value = self.linkedList.head.value
+        if self.linkedList.head == self.linkedList.tail:
+             self.linkedList.head=None
+             self.linkedList.tail=None
+        else:      
+          self.linkedList.head = self.linkedList.head.next
+        return value
+    def peek(self):
+       if self.is_empty():
+          return "Queue is empty"
+       else:
+          value = self.linkedList.head.value
+          return value
+    def delete(self):
+       self.linkedList.head=None
+       self.linkedList.tail=None
+
+customQueue = Queue()
+customQueue.enqueue(4)
+customQueue.enqueue(5)
+customQueue.enqueue(6)
+# print(customQueue)
+customQueue.dequeue()
+# print(customQueue.peek())
+print(customQueue)
--- QueueSize/create.py
+++ QueueSize/create.py
@@ -16,33 +16,59 @@ class Queue:
           return False
        
     def is_empty(self):
-        if self.list == []:
+        if self.top == -1:
             return True
         else:
             return False
     def enqueue(self,value):
-        self.list.append(value)
-        return "The element inserted successfully"
+      if self.is_full():
+        return "List is empty"
+      else:
+        if self.top+1 == self.maxSize:
+           self.top=0
+        else:
+           self.top+=1
+           if self.start ==-1:
+              self.start=0
+        self.list[self.top]=value
+        return f"Queue value inserted at {self.top} index"
     def dequeue(self):
       if self.is_empty():
         return "List is empty"
       else:
-        self.list.pop(0)
+        firstElement = self.list[self.start]
+        start = self.start
+        if self.start == self.top:
+           self.start =-1
+           self.top =-1
+        elif self.start+1==self.maxSize:
+           self.start=0
+        else:
+           self.start+=1
+        self.list[start]=None
+        return firstElement
     def peek(self):
       if self.is_empty():
         return "List is empty"
       else:
-        return self.list[0]
+        return self.list[self.start]
     def delete(self):
-        self.list=None
+        self.list=self.maxSize*[None]
+        self.top =-1
+        self.start=-1
 
-customQueue = Queue()
+customQueue = Queue(3)
 customQueue.enqueue(4)
 customQueue.enqueue(5)
 customQueue.enqueue(6)
+# print(customQueue)
+# customQueue.dequeue()
+# print(customQueue.peek())
 print(customQueue)
-customQueue.dequeue()
+print(customQueue.dequeue())
 print(customQueue.peek())
 print(customQueue)
-customQueue.delete()
-print(customQueue)
+# customQueue.delete()
+# print(customQueue.is_empty())
+# print(customQueue.is_full())
+
